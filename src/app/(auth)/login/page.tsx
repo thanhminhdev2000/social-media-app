6cb4e4f8095af3cc9c6380457474d42fbec47525
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-11-26 11:06:40 +0700

    Implement Sidebar and Fix ThemeProvider warning

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -25,6 +25,7 @@ export default function Page() {
           src={loginImage}
           alt=""
           className="hidden w-1/2 object-cover md:block"
+          priority
         />
       </div>
     </main>
