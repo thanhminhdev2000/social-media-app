6a98baaac85e30dbe6344ff5a6ed072986455f99
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-12-01 09:31:04 +0700

    Likes feature

---
+++ src/components/posts/LikeButton.tsx
@@ -0,0 +1,73 @@
+import { LikeInfo } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  QueryKey,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { Heart } from "lucide-react";
+import { useToast } from "../ui/use-toast";
+import axiosInstance from "@/lib/axios";
+
+interface LikeButtonProps {
+  postId: string;
+  initialState: LikeInfo;
+}
+
+export default function LikeButton({ postId, initialState }: LikeButtonProps) {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const queryKey: QueryKey = ["like-info", postId];
+
+  const { data } = useQuery<LikeInfo>({
+    queryKey,
+    queryFn: () => axiosInstance.get(`/api/posts/${postId}/likes`),
+    initialData: initialState,
+    staleTime: Infinity,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      data.isLikedByUser
+        ? axiosInstance.delete(`/api/posts/${postId}/likes`)
+        : axiosInstance.post(`/api/posts/${postId}/likes`),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<LikeInfo>(queryKey);
+
+      queryClient.setQueryData<LikeInfo>(queryKey, () => ({
+        likes:
+          (previousState?.likes || 0) + (previousState?.isLikedByUser ? -1 : 1),
+        isLikedByUser: !previousState?.isLikedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError(error, variables, context) {
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    },
+  });
+
+  return (
+    <button onClick={() => mutate()} className="flex items-center gap-2">
+      <Heart
+        className={cn(
+          "size-5",
+          data.isLikedByUser && "fill-red-500 text-red-500",
+        )}
+      />
+      <span className="text-sm font-medium tabular-nums">
+        {data.likes} <span className="hidden sm:inline">likes</span>
+      </span>
+    </button>
+  );
+}
