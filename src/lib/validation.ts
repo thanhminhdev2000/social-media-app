1d67a36701fbea18eab782aa7e67b167b1266c64
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-12-01 09:56:01 +0700

    Comment feature

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -29,4 +29,9 @@ export const updateUserProfileSchema = z.object({
   displayName: requiredString,
   bio: z.string().max(1000, "Must be at most 1000 characters"),
 });
+
 export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
+
+export const createCommentSchema = z.object({
+  content: requiredString,
+});
